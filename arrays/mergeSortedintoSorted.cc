ebe43b4d229a836cfdfd73bba18a8d02a25dcbd8
thepkd <dheeraj.putrevu@gmail.com>
2020-11-14 18:06:08 -0500

    Small needless modification

--- arrays/mergeSortedintoSorted.cc
+++ arrays/mergeSortedintoSorted.cc
@@ -3,13 +3,13 @@ int mergeSortedintoSorted(vector<int> nums1, int m, vector<int> nums2, int n)
     for(int i=0; i<n; i++)
     {
         int temp = nums[i];
-        for(j=0; j<m+i+1;j++)
+        for(int j=0; j<m+i+1;j++)
         {
             if(j!=m+i)
             {
             if(temp < nums1[j])
             {
-                for(k=m+j-1;k>j;k--)
+                for(int k=m+j-1;k>j;k--)
                 {
                     nums1[k] = nums1[k-1];
                 }
