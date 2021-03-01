9262eaed5ed6bdcd21903291aca67a2f75a0a9e3
thepkd <dheeraj.putrevu@gmail.com>
2021-03-01 12:34:30 -0500

    Update with memo-isation

--- stack_queue/numSums.cc
+++ stack_queue/numSums.cc
@@ -1,24 +1,34 @@
-int numSums(vector<int> nums, int S){
-    int curSum=0;
-    int yesses = 0;
+class Solution {
+	public:
+		    int findTargetSumWays(vector<int> nums, int S){
+			        int curSum=0;
+				    int yesses = 0;
+				        vector<vector<int>> memoSum(nums.size(), vector<int> (2000,INT_MIN));
+					    yesses = pushAndCheck(0, nums, curSum, S, memoSum);
+					        return yesses;
+		    }
 
-}
+		        int pushAndCheck(int level, vector<int> nums, int curSum, int& target, vector<vector<int>>& memoSum){
 
-void pushAndCheck(int level){
-    if(level == nums.size())
-    {
-        if(curSum==S)  yesses++;
-        return;
-        else
-        return; 
-    }
-    
-    curSum += nums[level];
-    pushAndCheck(level+1)
-    curSum -= nums[level];
-    curSum += (-nums[level]);
-    pushAndCheck(level+1);
-    curSum -= (-nums[level]);
-
-    return;
-}
+				    if(level == nums.size())
+					        {
+							        if(curSum==target)
+									        return 1;
+								        else
+										        return 0; 
+									    }
+				        else{
+						        if(memoSum[level][curSum+1000] != INT_MIN)
+								            return memoSum[level][curSum + 1000];
+							    }
+					    
+					    
+					    int  plus = pushAndCheck(level+1, nums, curSum + nums[level], target, memoSum);
+					        
+					        int minus = pushAndCheck(level+1, nums, curSum - nums[level], target, memoSum);
+						    
+						    memoSum[level][1000 + curSum] = plus + minus;
+						        return plus + minus;
+							    }
+			    
+};
